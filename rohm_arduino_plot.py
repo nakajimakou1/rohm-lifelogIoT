9b45a296a9b97a9272ed4a8a7763bdc66acbb9b8
nakajimakou1 <nakajimakou1@gmail.com>
2020-11-09 03:53:59 +0900

    加速度値の読み出し対応、地磁気の開店角表示対応

--- rohm_arduino_plot.py
+++ rohm_arduino_plot.py
@@ -5,13 +5,18 @@ import pygame
 from pygame.locals import *
 import serial
 import sys
+import math
 
 def main():
-    xscale = 400
+    xscale = 1000
     ser = serial.Serial("COM7", 115200)  # COMポート(Arduino接続)
     temps = np.array([0]*xscale)         # 温度格納
     myakus= np.array([0]*xscale)
     presss= np.array([0]*xscale)
+    magxys= np.array([0]*xscale)
+    accxs= np.array([0]*xscale)
+    accys= np.array([0]*xscale)
+    acczs= np.array([0]*xscale)
     
     t = np.arange(0,xscale,1)
     plt.ion()
@@ -27,11 +32,13 @@ def main():
     fig, ax = plt.subplots(1, 1)
 
     """
-    bm1383aglv.get_val(&press, &press_temp);
-    bd1020.get_val(&temp);
-    bm1422agmv.get_val(mag);
-    bh1790glc.get_val(val);
-    sprintf(str,"%d,%d,%s,%s,%s,%s,%s,%s\n", val[0], val[1], tempbuf, mag0buf, mag1buf, mag2buf, pressbuf, presstempbuf);
+    bm1383aglv.get_val(&press, &press_temp);// 気圧センサ
+    bd1020.get_val(&temp);	// 温度センサ
+    bm1422agmv.get_val(mag);// 地磁気センサ
+    bh1790glc.get_val(val);	// 脈波センサ
+    kx224.get_val(acc);		// 加速度センサ
+    sprintf(str,"%d,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", val[0], val[1], tempbuf, mag0buf, mag1buf, mag2buf, pressbuf, presstempbuf, acc0buf, acc1buf, acc2buf);
+    sprintf(str,"%d,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", val[0], val[1], tempbuf, mag[x] , mag[y] , mag[z] , pressbuf, presstempbuf, acc[x] , acc[y] , acc[z] );
     """
     ser.flush()
     while True:
@@ -41,7 +48,7 @@ def main():
         stemp = ser.readline().rstrip().decode(encoding='utf-8').replace(' ','') # \nまで読み込む
         temp = stemp.split(",") #.decode(encoding='utf-8')
         # print("[*] len(temp):",len(temp))
-        if(len(temp) == 8) :
+        if(len(temp) == 11) :
             print("[*1] temp:", temp)
         else:
             print("[*1] temp:+")
@@ -53,10 +60,22 @@ def main():
         # 温度データのリスト更新
         temps  = np.append(float(temp[2]), temps)    # np.array
         temps  = np.delete(temps, len(temps) - 1)
-        myakus = np.append(int(temp[0]), myakus)  
+        myakus = np.append(int(temp[0])+50, myakus)  
         myakus = np.delete(myakus, len(myakus) - 1)
-        presss = np.append((float(temp[6])-1021)*100, presss)  
+        presss = np.append((float(temp[6])-1009.2)*100, presss)  
         presss = np.delete(presss, len(presss) - 1)
+        magxys = np.append(math.degrees(math.atan2((float(temp[3])+30), (float(temp[4])+20))), magxys)  # 
+        #magxys = np.append(math.degrees(math.atan2((float(temp[4])+18), (float(temp[3])+30))), magxys)  # 
+        magxys = np.delete(magxys, len(magxys) - 1)
+        accxs  = np.append(float(temp[8])*50, accxs)
+        accxs  = np.delete(accxs, len(accxs) - 1)
+        accys  = np.append(float(temp[9])*50, accys)
+        accys  = np.delete(accys, len(accys) - 1)
+        acczs  = np.append(float(temp[10])*50, acczs)
+        acczs  = np.delete(acczs, len(acczs) - 1)
+
+        print("[*1] temp:", temp, magxys[0])
+
 
         # グラフ表示設定
         plt.title("Real-time temperature")
@@ -65,11 +84,14 @@ def main():
         plt.legend("t")
         plt.grid()
         plt.xlim([1,xscale])
-        plt.ylim([0,80])
+        plt.ylim([0,150])
         
-        line,  = ax.plot(t, temps, color='blue')   # Y軸更新
-        line2, = ax.plot(t, myakus, color='red')   # Y軸更新
-        line3, = ax.plot(t, presss, color='green') # Y軸更新
+        line,  = ax.plot(t, temps, color='blue')   # 温度更新
+        line2, = ax.plot(t, myakus, color='red')   # 脈波更新
+        line3, = ax.plot(t, presss, color='green') # 気圧更新
+        #line4, = ax.plot(t, accxs, color='yellow') # ACC X軸更新
+        line5, = ax.plot(t, acczs, color='purple') # ACC Z軸更新
+        line6, = ax.plot(t, magxys, color='black') # MAG XYRotate軸更新
 
         plt.draw()
         plt.pause(0.001)
@@ -78,6 +100,9 @@ def main():
         line.remove()
         line2.remove()
         line3.remove()
+        #line4.remove()
+        line5.remove()
+        line6.remove()
         
         for event in pygame.event.get():
         # 終了ボタンが押されたら終了処理
--- rohm_evk-003_life-log-sensor/rohm_evk-003_life-log-sensor.ino
+++ rohm_evk-003_life-log-sensor/rohm_evk-003_life-log-sensor.ino
@@ -32,24 +32,34 @@
 #include <BH1790GLC.h>
 #include <FlexiTimer2.h>
 #include <stdio.h>
+
+// 脈波センサ
 BH1790GLC bh1790glc;
 volatile bool timer_flg;
 char str[100];
 void timer_isr(void);
 
+// 温度センサ
 #include <BD1020.h>
 int tempout_pin = A0;
 BD1020 bd1020;
 
+// 地磁気センサ
 #include <BM1422AGMV.h>
 BM1422AGMV bm1422agmv(BM1422AGMV_DEVICE_ADDRESS_0E);
 void bm1422agmv_isr(void);
 
+// 気圧センサ
 #include <BM1383AGLV.h>
 BM1383AGLV bm1383aglv;
 
+// 加速度センサ
+#include <KX224_I2C.h>
+KX224 kx224(KX224_DEVICE_ADDRESS_1E);
+
 void setup() {
   byte rc;
+  float acc[3];
   
   timer_flg = false;
   Serial.begin(115200);
@@ -68,9 +78,15 @@ void setup() {
     bm1422agmv.isr_func(0, bm1422agmv_isr);
   }
 
-  rc = bh1790glc.init();
+  rc = kx224.init();
   if (rc != 0) {
-    Serial.println("BH1790 initialize failed");
+    Serial.println(F("KX224 initialization failed"));
+    Serial.flush();
+  }
+
+  rc = bh1790glc.init();// 脈波センサ
+  if (rc != 0) {
+    //Serial.println("BH1790 initialize failed");
   } else {
     FlexiTimer2::stop();
     FlexiTimer2::set(250, 1.0/8000, timer_isr);  // 32Hz timer
@@ -83,6 +99,7 @@ void loop() {
   unsigned short val[2];
   float temp;
   float mag[3];
+  float acc[3];
   float press = 0, press_temp = 0;
   char tempbuf[16];
   char mag0buf[16];
@@ -90,17 +107,26 @@ void loop() {
   char mag2buf[16];
   char pressbuf[16];
   char presstempbuf[16];
+  char acc0buf[16];
+  char acc1buf[16];
+  char acc2buf[16];
 
-  rc = bm1383aglv.get_val(&press, &press_temp);
+  rc = bm1383aglv.get_val(&press, &press_temp);// 気圧センサ
 
-  bd1020.get_val(&temp);
-  rc = bm1422agmv.get_val(mag);
+  rc = kx224.get_val(acc);    //加速度センサ
   if (rc == 0) {
-      //sprintf(str, "%d,%d,%s,%s,%s,%s,%s,%s\n", val[0], val[1], tempbuf, mag0buf, mag1buf, mag2buf, pressbuf, presstempbuf);
+      //sprintf(str, "KX224 (X):%d[g] (Y):%d[g] (Z):%d[g]\n", acc[0], acc[1], acc[2]);
+      //Serial.print(str);
+  }
+  
+  bd1020.get_val(&temp);        // 温度センサ
+  rc = bm1422agmv.get_val(mag); // 地磁気センサ
+  if (rc == 0) {
+      //sprintf(str, "%d,%d,%s,%s,%s,%s,%s,%s,%d,%d,%d\n", val[0], val[1], tempbuf, mag0buf, mag1buf, mag2buf, pressbuf, presstempbuf, acc[0], acc[1], acc[2]);
       //Serial.print(str);
   }
   if (timer_flg) {
-    rc = bh1790glc.get_val(val);
+    rc = bh1790glc.get_val(val);// 脈波センサ
     if (rc == 0) {
       dtostrf(temp,5,1,tempbuf);
       dtostrf(mag[0],5,1,mag0buf);
@@ -108,7 +134,10 @@ void loop() {
       dtostrf(mag[2],5,1,mag2buf);
       dtostrf(press,5,3 ,pressbuf);
       dtostrf(press_temp,5,1,presstempbuf);
-      sprintf(str, "%d,%d,%s,%s,%s,%s,%s,%s\n", val[0], val[1], tempbuf, mag0buf, mag1buf, mag2buf, pressbuf, presstempbuf);
+      dtostrf(acc[0],5,1,acc0buf);
+      dtostrf(acc[1],5,1,acc1buf);
+      dtostrf(acc[2],5,1,acc2buf);
+      sprintf(str, "%d,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", val[0], val[1], tempbuf, mag0buf, mag1buf, mag2buf, pressbuf, presstempbuf, acc0buf, acc1buf, acc2buf);
       Serial.print(str);
     }
     timer_flg = false;
